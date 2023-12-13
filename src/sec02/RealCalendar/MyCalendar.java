28a4fa083dfb23325a25db15d7e79cca6b437e19
JHLee <term273729@gmail.com>
2023-12-13 17:43:30 +0900

    정교한 달력찍는 작업 완성!

--- src/sec02/RealCalendar/MyCalendar.java
+++ src/sec02/RealCalendar/MyCalendar.java
@@ -1,12 +1,14 @@
 package sec02.RealCalendar;
 
 public class MyCalendar {
+    // 연월 셋팅
     final int[] MonthDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
     final int[] LeapMonthDays = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
     boolean isLeapDay(int year) { // 윤년이면 true를 반환하는 메소드
         return (year % 4 == 0 || (year % 100 == 0 && year % 400 == 0));
     }
 
+    // 윤년 처리
     int GetMonthDays(int year, int month) {
         int maxDay;
         if (isLeapDay(year)){
@@ -17,6 +19,7 @@ public class MyCalendar {
         }
     }
 
+    // 시작 요일 인덱스 처리
     int GetWeekDayIdx(String weekday) { //앞으로 첫글자는 소문자로 쓰자.
         if ("SU".equals(weekday)) { // 문자열 비교는 == 말고 자바에선 equals 메소드를 써야함.
             return 0;
@@ -44,18 +47,32 @@ public class MyCalendar {
         }
     }
 
+    // 연도, 월, 시작 요일을 기반으로 달력을 출력
     void Cal(int year, int month, String weekday) {
         System.out.printf("    <<%d년 %d월>>\n", year, month);
         System.out.println(" SU MO TU WE TH FR SA");
         System.out.println("----------------------");
         int weekDayIdx = GetWeekDayIdx(weekday);
-        System.out.print("   ".repeat(weekDayIdx)); //Java 11 이상부터 사용가능, Java 11 이전의 경우 for문 돌려야 함.
+        for (int i = 0; i < weekDayIdx; i++) {
+            System.out.print("   ");
+        }
 
         int maxDay = GetMonthDays(year, month);
+        int count = 7 - weekDayIdx; // WE 입력한 경우 7 - IDX(=3) = 4
 
-        for (int i = 1; i <= maxDay; i++) {
+        //FIRST LINE PRINT
+        for (int i = 1; i <= count; i++) {
             System.out.printf("%3d", i);
-            if (i % 7 == 0) {
+        }
+        System.out.println();
+
+        int cnt = 0;
+        //SECOND TO LAST PRINT
+        for (int i = count + 1; i <= maxDay; i++) {
+            cnt += 1;
+            System.out.printf("%3d", i);
+
+            if (cnt % 7 == 0) {
                 System.out.println();
             }
         }
