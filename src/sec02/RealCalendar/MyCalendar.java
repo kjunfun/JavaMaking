2cd102e662efdc4af23153d5c46e9082343ba8aa
JHLee <term273729@gmail.com>
2023-12-15 18:19:57 +0900

    auto complete calendar(fixed-ver)

--- src/sec02/RealCalendar/MyCalendar.java
+++ src/sec02/RealCalendar/MyCalendar.java
@@ -1,9 +1,5 @@
 package sec02.RealCalendar;
 /* <TIL>
-* 일단 함수를 호출 형태로 먼저 써주고, InteliJ에 의존하여 메소드는 자동생성. 추상화를 위해 생성되는 메소드는 아래에 깔아둠.
-* 윤년, 그레고리력, 기준 연월일 배경지식을 이해하고, 첫번째 요일 계산 메소드 작성 후 호출하는 방식으로 정교한 달력 구현 완료.
-* 여기서부터 다시 시작: getWeekDay 메소드 완성하면 됨.
-- idx 0, 1, 2, 3 이런 부분들을 어떻게 처리했는지 이해하기.
 */
 public class MyCalendar {
     // 윤년(=그레고리력에서 사용되는 양력의 개념중 하나로, 태양년의 길이를 정확하게 반영하기 위한 조정. why? 태양년의 지구 공전 시간은 365.2422정도 이므로, 1년 365일 달력으로는 태양년 길이를 정확히 반영할 수 없으므로 4년에 1일씩 추가하는 것임)일 경우 2월이 하루 더 늘어난 29일이 된다.
@@ -17,10 +13,10 @@ public class MyCalendar {
     // 구현된 메소드 사용하여 년도, 월 입력하면 정교한 달력을 출력
     void Cal(int year, int month) {
         System.out.printf("   <%d년 %d월 달력>\n", year, month);
-        System.out.println("SU MO TU WE TH FR SA");
+        System.out.println(" SU MO TU WE TH FR SA");
         System.out.println("----------------------");
         // 자동으로 첫번째 요일 가져오기
-        int weekDay = getWeekDay(year, month, 1);
+        int weekDay = getWeekDay(year, month);
         for (int i = 0; i < weekDay; i++) {
             System.out.print("   ");
         }
@@ -46,7 +42,6 @@ public class MyCalendar {
 
     // 메소드 구현 부분
     int GetMonthDays(int year, int month) {
-        int maxDay;
         if (isLeapDay(year)){
             return LeapMonthDays[month-1];
         }
@@ -55,17 +50,30 @@ public class MyCalendar {
         }
     }
 
-    int getWeekDay(int year, int month, int day) {
-        final int standard_year = 1970; // 1970년 1월 1일 목요일(일요일:0, 월요일:1,,, 목요일:4)
+    int getWeekDay(int year, int month) {
+        final int standard_year = 1970; // 1970년 1월 1일 목요일(일요일:0,,, 목요일:4)
         final int standard_weekday = 4;
         int day_count = 0;
-        for (int i = standard_year; i < standard_year; i++) {
+        for (int i = standard_year; i < year; i++) {
             int for_sum = isLeapDay(i) ? 366 : 365;
             day_count += for_sum;
         }
-        for (int i = 0; i < month; i++) {
+        for (int i = 1; i < month; i++) {
             int for_sum = GetMonthDays(year, i);
             day_count += for_sum;
         }
+        int weekday;
+        weekday = (day_count + standard_weekday) % 7;
+        return weekday;
+    }
+
+    // simple test code
+    public static void main(String[] args) {
+        MyCalendar c = new MyCalendar();
+        System.out.println(c.getWeekDay(1970, 1));
+        System.out.println(c.getWeekDay(1971, 1));
+        System.out.println(c.getWeekDay(1972, 1));
+        System.out.println(c.getWeekDay(1973, 1));
+        System.out.println(c.getWeekDay(1974, 1));
     }
 }
